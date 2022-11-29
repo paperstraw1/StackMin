d24c703a8842c2ab7adf9ed7a85a62291d62f678
paperstraw1 <nicholas.adelmund@cuw.edu>
2022-11-28 19:00:45 -0600

    hw11

---
+++ src/StackMin/main.java
@@ -0,0 +1,15 @@
+package StackMin;
+
+public class main
+{ 
+	public static void main(String[] args) {
+    stackmin stack = new stackmin();
+    
+    stack.push(5);    
+    stack.push(6);
+    stack.push(1);
+    stack.push(8);
+    
+    stack.pop();
+	}
+}
---
+++ src/StackMin/stackmin.java
@@ -0,0 +1,54 @@
+package StackMin;
+import java.util.Stack;
+
+public class stackmin {
+	  Stack<Integer> stack;
+	    int minimum;
+
+      stackmin() { 
+	    	stack = new Stack<Integer>(); 
+	    }
+	 
+	    void push(Integer i)
+	    {
+	        if (stack.isEmpty()) {
+	            minimum = i;
+	            stack.push(i);
+	            System.out.println("The number inserted: " + i);
+	            return;
+	        }
+
+	        if (i < minimum) {
+	            stack.push(2 * i - minimum);
+	            minimum = i;
+	        } else
+	            stack.push(i);
+	 
+	        System.out.println("The number inserted: " + i);
+	    }
+
+	    void pop()
+	    {
+	        if (stack.isEmpty()) {
+	            System.out.println("There is nothing in the stack.");
+	            return;
+	        }
+	 
+	        System.out.print("The top element REMOVED: ");
+	        Integer x = stack.pop();
+	 
+	        if (x < minimum) {
+	            System.out.println(minimum);
+	            minimum = 2 * minimum - x;
+	        } else
+	            System.out.println(x);
+	    }
+	    
+	    void minimum()
+	    {
+	        if (stack.isEmpty())
+	            System.out.println("There is nothing in the stack.");
+	        else
+	            System.out.println("The minimum element in the stack is: " + minimum);
+	    }
+	}
